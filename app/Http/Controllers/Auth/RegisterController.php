490490f4005b098b8f2df700d654af1d7e48e4a9
Wiercinski <pawel.wiercinski@fujitsu.com>
2021-01-22 10:31:17 +0100

    Register auth controller, Dashboard route and controller, verification

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,12 +2,33 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
     public function index() {
         return view('auth.register');
+    }
+    public function store(Request $request) {
+        $this->validate($request, [
+            'name' => 'required|max:20',
+            'username' => 'required|max:10',
+            'email' => 'required|max:50',
+            'password' => 'required|max:20|confirmed',
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        auth()->attempt($request->only('email', 'password'));
+
+        return redirect()->route('dashboard');
     }
 }
